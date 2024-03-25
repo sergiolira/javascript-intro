03763dcccfa8cb4ab12641488f5c5eb3a205be74
sergiolira <seerghio.lira@gmail.com>
2024-03-24 19:43:40 -0500

    var, const y let

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
+/**Const**/
+const name = 'Sergio'
+const lastname = 'lira'
 
+console.log(`Hello ${name} ${lastname}`)
 
+if(true){
+    const name ='Emely'
+    console.log(`Hello ${name} ${lastname}`)
+}
+/**Let */
+let fulname = 'Sergio lira'
+console.log(fulname)
 
-
-
-
-console.log('Hola Mundo!!')
-
+if(true){
+    fulname = 'Sandra liras'
+    console.log(fulname)
+}
 
 
 
